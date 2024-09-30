f839149da8f766b1648ab902af17252c6e75bc24
arrolin-dev <ivan.de.baere@arrolin.com>
2024-09-30 15:00:17 +0200

    list add

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,21 +4,21 @@ class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "Estate Property"
 
-    name = fields.Char(required=True, default="Unknown")
+    name = fields.Char(string='Title',required=True, default="Unknown")
     active = fields.Boolean(default=True)
     state = fields.Selection(
         selection=[('new','New'),('offer_received','Offer Received'),('offer_accepted','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')],
         default='new'
     )
     last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
-    
+
     description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date(copy=False,default=fields.Date.add(value = fields.Date.today(),months=3))
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float(readonly=True,copy=False)
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(string='Available From',copy=False,default=fields.Date.add(value = fields.Date.today(),months=3))
+    expected_price = fields.Float(string='Expected Price', required=True)
+    selling_price = fields.Float(string='Selling Price',readonly=True,copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
