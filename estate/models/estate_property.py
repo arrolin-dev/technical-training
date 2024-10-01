2b4605bafb5edaae7d6aeccba5aa98ec02d077bf
arrolin-dev <ivan.de.baere@arrolin.com>
2024-10-01 09:24:46 +0200

    add offer and tag

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -24,7 +24,7 @@ class EstateProperty(models.Model):
     salesperson_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
     byer_id = fields.Many2one("res.partner",string="Byer", copy=False)
     tag_ids = fields.Many2many("estate.property.tag")
-    #offer_ids = fields.One2many("estate.property.offer", "property_id")
+    offer_ids = fields.One2many("estate.property.offer", "property_id")
 
 
     # Public Attributes
